db8fd122de84c446bc2acaa74ba55996bec9c314
jamesdowty <73566663+jamesdowty@users.noreply.github.com>
2020-11-05 18:07:52 -0800

    Comments
    Added a comment with project name and team members.

--- CS1C_Graphics_Modeler/main.cpp
+++ CS1C_Graphics_Modeler/main.cpp
@@ -1,3 +1,10 @@
+/**************************************************************
+ * CS1C Group Project
+ * Team Name: Scrum and Coke
+ * Team Members: James Dowty, Joey Campbell, Behrad Moussavi,
+ *               Ahazz Khan, Kate Steer, Utsav Parajuli,
+ *               Nick Lozano, Brandon Nguyen, and Tim Diersing
+ * ***********************************************************/
 #include "mainwindow.h"
 
 #include <QApplication>
