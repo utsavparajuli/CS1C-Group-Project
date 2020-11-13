8281e33b26ee907e0c50b51165159df564596731
jamesdowty <73566663+jamesdowty@users.noreply.github.com>
2020-11-12 17:27:46 -0800

    Parser outline

--- CS1C_Graphics_Modeler/parser.cpp
+++ CS1C_Graphics_Modeler/parser.cpp
@@ -6,10 +6,13 @@
 
 void parser(const QString fileName)
 {
-    QString file = qApp->applicationDirPath();  //Setting file to the path of the .exe file
-    file.append('/' + fileName);  //appending the file name to the path
+    int ShapeID = 1;
 
-    QFile inputFile(file);    //opening the input file located at the path
+    //Storing the path to the .txt file in filePath
+    QString filePath = qApp->applicationDirPath();
+    filePath.append('/' + fileName);
+
+    QFile inputFile(filePath); //opening the input file located at the path
 
     //Error checking if file exists.
     //If it does not, a QMessage box will pop up with the file name.
@@ -22,5 +25,152 @@ void parser(const QString fileName)
     }
 
     QTextStream input(&inputFile);
-    qDebug() << input.readLine();
+    QString textLine;
+
+    while(!input.atEnd())
+    {
+        input.readLine();  //Reading in the first blank line
+        input.readLine(); //Throwing away shapeID (shapes will be numbered in the order they are read in)
+        textLine = input.readLine().remove(0, 11); //Reading in shape type
+
+        if(textLine == "Line")
+        {
+            qDebug() << "\nPrinting Line";
+            ParseLine(input, ShapeID);
+        }
+        else if(textLine == "Polyline")
+        {
+            qDebug() << "\nPrinting Polyline";
+            ParsePolyline(input, ShapeID);
+        }
+        else if(textLine == "Polygon")
+        {
+            qDebug() << "\nPrinting Polygon";
+            ParsePolygon(input, ShapeID);
+        }
+        else if(textLine == "Rectangle")
+        {
+            qDebug() << "\nPrinting Rectangle";
+            ParseRectangle(input, ShapeID);
+        }
+        else if(textLine == "Square")
+        {
+            qDebug() << "\nPrinting Square";
+            ParseSquare(input, ShapeID);
+        }
+        else if(textLine == "Ellipse")
+        {
+            qDebug() << "\nPrinting Ellipse";
+            ParseEllipse(input, ShapeID);
+        }
+        else if(textLine == "Circle")
+        {
+            qDebug() << "\nPrinting Circle";
+            ParseCircle(input, ShapeID);
+        }
+        else if(textLine == "Text")
+        {
+            qDebug() << "\nPrinting Text";
+            ParseText(input, ShapeID);
+        }
+        else
+        {
+            qDebug() << "ERROR: Shape type " << textLine << " is not valid.";
+        }
+
+        ShapeID++;
+    }
+
+}
+
+void ParseLine(QTextStream &file, int ShapeID)
+{
+    qDebug() << file.readLine();
+    qDebug() << file.readLine();
+    qDebug() << file.readLine();
+    qDebug() << file.readLine();
+    qDebug() << file.readLine();
+    qDebug() << file.readLine();
+}
+
+void ParsePolyline(QTextStream &file, int ShapeID)
+{
+    qDebug() << file.readLine();
+    qDebug() << file.readLine();
+    qDebug() << file.readLine();
+    qDebug() << file.readLine();
+    qDebug() << file.readLine();
+    qDebug() << file.readLine();
+}
+
+void ParsePolygon(QTextStream &file, int ShapeID)
+{
+    qDebug() << file.readLine();
+    qDebug() << file.readLine();
+    qDebug() << file.readLine();
+    qDebug() << file.readLine();
+    qDebug() << file.readLine();
+    qDebug() << file.readLine();
+    qDebug() << file.readLine();
+    qDebug() << file.readLine();
+}
+
+void ParseRectangle(QTextStream &file, int ShapeID)
+{
+    qDebug() << file.readLine();
+    qDebug() << file.readLine();
+    qDebug() << file.readLine();
+    qDebug() << file.readLine();
+    qDebug() << file.readLine();
+    qDebug() << file.readLine();
+    qDebug() << file.readLine();
+    qDebug() << file.readLine();
+}
+
+void ParseSquare(QTextStream &file, int ShapeID)
+{
+    qDebug() << file.readLine();
+    qDebug() << file.readLine();
+    qDebug() << file.readLine();
+    qDebug() << file.readLine();
+    qDebug() << file.readLine();
+    qDebug() << file.readLine();
+    qDebug() << file.readLine();
+    qDebug() << file.readLine();
+}
+
+void ParseEllipse(QTextStream &file, int ShapeID)
+{
+    qDebug() << file.readLine();
+    qDebug() << file.readLine();
+    qDebug() << file.readLine();
+    qDebug() << file.readLine();
+    qDebug() << file.readLine();
+    qDebug() << file.readLine();
+    qDebug() << file.readLine();
+    qDebug() << file.readLine();
+}
+
+void ParseCircle(QTextStream &file, int ShapeID)
+{
+    qDebug() << file.readLine();
+    qDebug() << file.readLine();
+    qDebug() << file.readLine();
+    qDebug() << file.readLine();
+    qDebug() << file.readLine();
+    qDebug() << file.readLine();
+    qDebug() << file.readLine();
+    qDebug() << file.readLine();
+}
+
+void ParseText(QTextStream &file, int ShapeID)
+{
+    qDebug() << file.readLine();
+    qDebug() << file.readLine();
+    qDebug() << file.readLine();
+    qDebug() << file.readLine();
+    qDebug() << file.readLine();
+    qDebug() << file.readLine();
+    qDebug() << file.readLine();
+    qDebug() << file.readLine();
 }
--- CS1C_Graphics_Modeler/parser.h
+++ CS1C_Graphics_Modeler/parser.h
@@ -5,8 +5,18 @@
 #include "shape.h"
 #include <QString>
 #include <QFile>
+#include <QTextStream>
 
 //void shapeParser(vector<shape*>&, const QString fileName); //PROTOTYPE
+
 void parser(const QString fileName);
+void ParseLine(QTextStream &file, int ShapeID);
+void ParsePolyline(QTextStream &file, int ShapeID);
+void ParsePolygon(QTextStream &file, int ShapeID);
+void ParseRectangle(QTextStream &file, int ShapeID);
+void ParseSquare(QTextStream &file, int ShapeID);
+void ParseEllipse(QTextStream &file, int ShapeID);
+void ParseCircle(QTextStream &file, int ShapeID);
+void ParseText(QTextStream &file, int ShapeID);
 
 #endif // PARSER_H
