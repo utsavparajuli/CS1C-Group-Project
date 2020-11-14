96bdaa0bf59762b47e6fd733f090cf98f3b80527
utsavparajuli <73618451+utsavparajuli@users.noreply.github.com>
2020-11-13 18:59:38 -0800

    Updating shape.cpp
    Still need to implement the less than and equality operator overloading

--- CS1C_Graphics_Modeler/shape.cpp
+++ CS1C_Graphics_Modeler/shape.cpp
@@ -1,5 +1,6 @@
 #include "shape.h"
 
+//constructor
 shape::shape(QPaintDevice* device, int id, ShapeType shape)
     :qpainter{device}, shapeId{id}, shapeType{shape}
 {
@@ -8,43 +9,53 @@ shape::shape(QPaintDevice* device, int id, ShapeType shape)
 }
 
 
+//returns the shape
 ShapeType shape::get_shape() const
 {
     return shapeType;
 }
 
 
+//returns the qpainter
 QPainter& shape::get_qpainter()
 {
     return qpainter;
 }
 
+
+//returns the pen
 const QPen& shape::get_pen() const
 {
     return pen;
 }
 
+
+//returns the brush
 const QBrush& shape::get_brush() const
 {
     return brush;
 }
 
+//sets the id of the shape
 void shape::set_ShapeId(int id)
 {
     this->shapeId = id;
 }
 
+//sets the type of shape
 void shape::set_Shape(ShapeType shape)
 {
     shapeType = shape;
 }
 
+//sets the brush
 void shape::set_brush(Qt::GlobalColor color, Qt::BrushStyle brushStyle)
 {
     brush.setColor(color);
     brush.setStyle(brushStyle);
 }
 
+//sets the pen
 void shape::set_pen(Qt::GlobalColor color, int width, Qt::PenStyle penStyle, Qt::PenCapStyle penCapStyle, Qt::PenJoinStyle penJoinStyle)
 {
     pen.setColor(color);
@@ -54,11 +65,13 @@ void shape::set_pen(Qt::GlobalColor color, int width, Qt::PenStyle penStyle, Qt:
     pen.setJoinStyle(penJoinStyle);
 }
 
+//sets the pens color
 void shape::set_pen(Qt::GlobalColor color)
 {
     pen.setColor(color);
 }
 
+//default style for shape
 void shape::default_style()
 {
     pen = Qt::SolidLine;
@@ -68,21 +81,8 @@ void shape::default_style()
     qpainter.setBrush(brush);
 }
 
+//drawing rectagnle
 void shape::draw_rect(int width, int height)
 {
     qpainter.drawRect(QRect(0, 0, width, height));
 }
-
-
-//void shape::set_text(string text, Qt::GlobalColor color, Qt::AlignmentFlag alignment, int pointSize, string fontFamily, QFont::Style style, QFont::Weight weight)
-//{
-//    font.setStyle(style);
-//    text.
-
-
-//}
-
-//void shape::set_text_color(Qt::GlobalColor color)
-//{
-//    //text.setStyleSheet
-//}
